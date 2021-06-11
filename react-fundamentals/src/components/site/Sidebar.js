593d32b91cf6ce1668d2fa7cd1da7bfbbea6dfe4
mrenee73 <michelecountryman1@gmail.com>
2021-06-11 14:57:41 -0400

    merging all the things

--- react-fundamentals/src/components/site/Sidebar.js
+++ react-fundamentals/src/components/site/Sidebar.js
@@ -4,14 +4,11 @@ import {
     Switch
 } from 'react-router-dom';
 import Home from './Home';
-<<<<<<< HEAD
 import signPic from '../../assets/neighborhoodsign.jpg';
-=======
 
-import neighborhood from '../'
->>>>>>> c4e3462d94cc7cfff395431fb2b7889915bb275c
 
-import Resource from ''
+
+
 
 
 const Sidebar = () => {
