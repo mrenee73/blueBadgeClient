103b0424ebf55c37b9c8a7f0b57dad31d37902cf
kcmjschwartz <kcmjschwartz@gmail.com>
2021-06-14 15:47:54 -0400

    user entry view update and delete added

--- react-fundamentals/src/components/ContactUs/ContactUs.js
+++ react-fundamentals/src/components/ContactUs/ContactUs.js
@@ -1,61 +1,41 @@
-import React, {useState} from 'react';
-import {Form, FormGroup, Label, Input, Button} from 'reactstrap';
-import './ContactUs.css'
+import React from 'react';
+import { useForm, ValidationError } from '@formspree/react';
 
-
-const contactUs = (props) => {
-    const[nameOfForm, setNameOfForm] = useState('');
-    const[emailOfForm, setEmailOfForm] = useState('');
-    const[dateOfForm, setDateOfForm] = useState('');
-    const[messageOfForm, setMessageOfForm] = useState('');
-    
-    
-    const sendMessage = e => {
-        e.preventDefault();
-
-        let url = 'http://localhost:4000/entry/';
-
-        fetch(url, {
-            method: 'POST',
-            body: JSON.stringify({log:{name: nameOfForm, email: emailOfForm, date: dateOfForm, message: messageOfForm}}),
-            headers: new Headers({
-                'Content-Type' : 'application/json',
-                'Authorization': props.sessionToken
-            })
-        })
-        .then((response) => response.json())
-        .then((result) =>{
-        console.log(result);
-        props.updateToken(result.sessionToken);
-    }).catch(err => console.log(err));
-
-    }
-
-    return(
-        <div>
-            <h1>Contact Us</h1>
-                <Form onSubmit={sendMessage}>
-                    <FormGroup>
-                        <Label htmlFor="nameOfForm">Name</Label>
-                        <Input name="nameOfForm" value={Name} onChange={(e) => setNameOfForm(e.target.value)}/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label htmlFor="dateOfForm">Date</Label>
-                        <Input Type= "date" name="dateOfForm" value={date} onChange={(e) => setEmailOfForm(e.target.value)}/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label htmlFor="emailOfForm">Email</Label>
-                        <Input Type= "email" name="emailOfForm" value={email} onChange={(e) => setDateOfForm(e.target.value)}/>
-                    </FormGroup>
-                    <FormGroup>
-                        <Label htmlFor="messageOfForm">Message</Label>
-                        <Input name="messageOfForm" value = {Message} onChange={(e) => setMessageOfForm(e.target.value)}/>
-                    </FormGroup>
-                    <Button type="submit">Send Message</Button>
-                </Form>
-        </div>
-    )
-    
+function ContactForm() {
+  const [state, handleSubmit] = useForm("mwkabbzj");
+  if (state.succeeded) {
+      return <h1>Thanks for reaching out! We will contact you soon!</h1>;
+  }
+  return (
+      <form onSubmit={handleSubmit}>
+      <label htmlFor="email">
+        Email Address
+      </label>
+      <input
+        id="email"
+        type="email" 
+        name="email"
+      />
+      <ValidationError 
+        prefix="Email" 
+        field="email"
+        errors={state.errors}
+      />
+      <textarea
+        id="message"
+        name="message"
+      />
+      <ValidationError 
+        prefix="Message" 
+        field="message"
+        errors={state.errors}
+      />
+      <button type="submit" disabled={state.submitting}>
+        Submit
+      </button>
+    </form>
+  );
 }
 
-export default contactUs;
+
+export default ContactForm;
